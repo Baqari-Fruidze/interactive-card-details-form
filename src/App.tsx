701931dce862ac9da837c8c4bdec0d7999e508de
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-07-01 19:26:25 +0400

    start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useForm } from "react-hook-form";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  return <></>;
+}
+
+export default App;
