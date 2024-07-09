2838152817e14866963b4affc60158dfa3826ab0
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-07-09 19:44:34 +0400

    front fixed

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -5,10 +5,16 @@ export default function Front() {
   return (
     <div className="w-[285px] h-[156px] bg-card-front bg-no-repeat bg-[length:285px_156px] ml-[17px] mt-[-68px] pt-[17.6px] pr-[20.89px]  pl-[19px] ">
       <img src={logo} alt="" className="w-[54px] h-[30px] mb-[37px]" />
-      <p className="mb-[17px]"> 0000 0000 0000 0000 </p>
-      <div className="flex justify-between">
-        <span>JANE APPLESEED</span>
-        <span>00/00</span>
+      <p className="mb-[17px] text-[18px] text-[#fff] font-medium tracking-[2.2px]">
+        0000 0000 0000 0000
+      </p>
+      <div className="nameAndDate flex justify-between items-center">
+        <span className="text-[9px] text-[#fff] font-medium tracking-[1.286px] uppercase">
+          JANE APPLESEED
+        </span>
+        <span className="text-[9px] text-[#fff] font-medium tracking-[1.286px] uppercase">
+          00/00
+        </span>
       </div>
     </div>
   );
