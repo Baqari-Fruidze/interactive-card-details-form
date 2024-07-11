e8b3c06284334eebd54e5b53fc18db8cd5917727
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-07-11 14:10:49 +0400

    inputs first hals styled

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -12,12 +12,32 @@ export default function Inputs() {
     <div className="parent px-[24px] pt-[70px]">
       <form>
         <div className="nameCon flex flex-col">
-          <label htmlFor="name">Cardholder Name</label>
-          <input type="text" id="name" />
+          <label
+            htmlFor="name"
+            className="labelCommonStyles text-[12px] text-[#21092F] font-medium tracking-[2px] uppercase mb-[9px]"
+          >
+            Cardholder Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="e.g. Jane Appleseed"
+            className="rounded-[8px] border-[1px] border-[solid] border-[#DFDEE0] py-[10px] outline-none pl-[16px] mb-[20px] placeholder:opacity-25 placeholder:text-[18px] placeholder:font-medium placeholder:uppercase"
+          />
         </div>
         <div className="cardNumber flex flex-col">
-          <label htmlFor="cardNumber"> Card Number</label>
-          <input type="text" id="cardNumber" />
+          <label
+            htmlFor="cardNumber"
+            className="labelCommonStyles text-[12px] text-[#21092F] font-medium tracking-[2px] uppercase mb-[9px] "
+          >
+            Card Number
+          </label>
+          <input
+            type="text"
+            id="cardNumber"
+            placeholder="e.g. 1234 5678 9123 0000"
+            className="rounded-[8px] border-[1px] border-[solid] border-[#DFDEE0] py-[10px] outline-none pl-[16px] mb-[20px] placeholder:opacity-25 placeholder:text-[18px] placeholder:font-medium placeholder:uppercase"
+          />
         </div>
         <div>
           <div>
